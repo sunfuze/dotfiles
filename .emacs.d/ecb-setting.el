02d39312272945630773f0e110cd63c13f5b9cb1
sunfuze <sunfuze.1989@gmail.com>
2014-01-07 18:47:50 +0800

    install yasnippet by elpa

---
+++ .emacs.d/ecb-setting.el
@@ -0,0 +1,4 @@
+(setq ecb-layout-name "leftright2")
+(setq ecb-show-sources-in-directories-buffer 'always)
+(setq ecb-compile-window-height 12)
+(provide 'ecb-setting)
---
+++ .emacs.d/elpa/popup-20131230.544/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21195 44772
+;;;;;;  277216 931000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
